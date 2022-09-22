dd6c7667ae79ff1e577e7d8e406e377aa5b45490
Octavian Dobre <octavian.dobre@medel.com>
2022-09-22 16:55:01 +0200

    Layout V1

---
+++ Mission/MissionWebApp/wwwroot/js/jslogic.js
@@ -0,0 +1,12 @@
+﻿function loadCss(cssPath) {
+    var ss = document.styleSheets;
+    for (var i = 0, max = ss.length; i < max; i++) {
+        if (ss[i].href.includes(cssPath.substr(2)))
+            return;
+    }
+    var link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = cssPath;
+
+    document.getElementsByTagName("head")[0].appendChild(link);
+}
